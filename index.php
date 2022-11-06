71765989a533467d4061872a8a64d1b0f5132254
Chetny <mateuszchecinski97@gmail.com>
2022-11-06 14:07:19 +0100

    Added controller and changed code in index

--- index.php
+++ index.php
@@ -6,33 +6,9 @@ namespace App;
 //error_reporting(0);
 //ini_set('display_errors', '0');
 
-require_once('./src/view.php');
+require_once('./src/controller.php');
 include_once('./src/utils/debug.php');
 
-const DEFAULT_ACTION = 'list';
 
-$action = $_GET['action'] ?? DEFAULT_ACTION;
-
-$viewParams = [];
-
-switch ($action) {
-    case 'create':
-        $page = 'create';
-        $created = false;
-        if (!empty($_POST)) {
-            $viewParams = [
-                'title' => $_POST['title'],
-                'description' => $_POST['description'],
-            ];
-            $created = true;
-        }
-        $viewParams['created'] = $created;
-        break;
-    default:
-        $page = 'list';
-        $viewParams['resultList'] = 'Wyświetlamy listę notatek';
-        break;
-}
-
-$view = new View();
-$view->render($page, $viewParams);
+$controller = new Controller($_GET, $_POST);
+$controller->run();
