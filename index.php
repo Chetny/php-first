730918d523fb9cb8398ddcc8960e1ba4b468f390
Chetny <mateuszchecinski97@gmail.com>
2022-11-06 13:37:14 +0100

    Code issue fixed

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 namespace App;
 
+//w wersji na świat, dla nas błędy są ważne
+//error_reporting(0);
+//ini_set('display_errors', '0');
+
 require_once('./src/view.php');
 include_once('./src/utils/debug.php');
 
@@ -12,8 +16,10 @@ $action = $_GET['action'] ?? DEFAULT_ACTION;
 $viewParams = [];
 
 if ($action === 'create') {
+    $page = 'create';
     $viewParams['resultCreate'] = 'Udało sie dodać notatke';
 } else {
+    $page = 'list';
     $viewParams['resultList'] = 'Wyświetlamy liste notatek';
 }
 
